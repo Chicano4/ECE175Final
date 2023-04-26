1d65981bec3d7d22e53e6f8c40274ade66f92b33
Chicano4 <89478188+Chicano4@users.noreply.github.com>
2023-04-26 15:55:54 -0700

    Update FinalProject175.c

--- FinalProject175.c
+++ FinalProject175.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+// Functions needed: check card, addCard, DeleteCard, 
+typedef struct card_s {
+	char color[10];
+	int value;
+	char action[15];
+	struct card_s* pt;
+} card;
 int main(void) {
-	printf("thi runs");
+	FILE* inp = fopen("CardDeck.txt","r");
+	card deck[108];
+	int i,j;
+	while (!feof(inp)) {
+		for (i = 0; i < 108;i++) {
+			fscanf(inp, " %s", deck[i].color);
+			fscanf(inp, " %d", &deck[i].value);
+			fscanf(inp, " %s", deck[i].action);
+			
+		}
+	}
+	
+	return 0;
 }
