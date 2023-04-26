cdd1211cf23552d8bfea32958223ec9ae7ccdedb
Chicano4 <89478188+Chicano4@users.noreply.github.com>
2023-04-26 16:36:24 -0700

    Update FinalProject175.c

--- FinalProject175.c
+++ FinalProject175.c
@@ -7,12 +7,14 @@ typedef struct card_s {
 	char color[10];
 	int value;
 	char action[15];
-	struct card_s* pt;
+	struct card_s* pt;//pt is the equivalent of next, goes from head to tail of linked list
 } card;
+void printHand(card **h, card **t);
 int main(void) {
 	FILE* inp = fopen("CardDeck.txt","r");
 	card deck[108];
-	int i,j;
+	int i,currentCardPosition = 0;
+	card *p1Head = NULL, *p1Tail = NULL, *p2Head = NULL, *p2Tail = NULL, *centerHead = NULL, *centerTail = NULL;
 	while (!feof(inp)) {
 		for (i = 0; i < 108;i++) {
 			fscanf(inp, " %s", deck[i].color);
@@ -21,6 +23,23 @@ int main(void) {
 			
 		}
 	}
-	
+	for (int k = 0;k < 7;k++) {
+		addCard(&p1Head, &p1Tail, deck[currentCardPosition]);
+		currentCardPosition++;
+		addCard(&p2Head, &p2Tail, deck[currentCardPosition]);
+		currentCardPosition++;
+	}
+	addCard(&centerHead, &centerTail, deck[currentCardPosition]);
+	currentCardPosition++;
+	addCard(&centerHead, &centerTail, deck[currentCardPosition]);
+	currentCardPosition++;
+	printHand(p1Head);
 	return 0;
+}
+void printHand(card *h) {
+	card* i = h;
+	while (i != NULL) {
+		printf("%s %d %s\n", i->color, i->value, i->action);
+		i = i->pt;
+	}
 }
